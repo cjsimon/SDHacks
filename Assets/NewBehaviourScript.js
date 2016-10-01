3b199e40c63c0986a8275400680281f4db00b844
Allen Guan <apguan@gmail.com>
2016-10-01 12:26:29 -0700

    gets raw audio bytes

--- Assets/NewBehaviourScript.js
+++ Assets/NewBehaviourScript.js
@@ -12,6 +12,19 @@ function Start () {
 	for (var device in Microphone.devices){
 			Debug.Log("Name: " + device);
 	}
+
+//for raw data stuff
+
+	var samples = new float[aud.clip.samples * aud.clip.channels];
+	aud.clip.GetData(samples, 0);
+	
+	for (var i = 0; i < samples.Length; ++i){
+		samples[i] = samples[i] * 0.5f;
+	}
+
+	aud.clip.SetData(samples, 0);
+
+
 }
 
 function Update () {
