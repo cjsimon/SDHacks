5e0d438bba21d1d1179fe9f81e1e0db96e17e4f4
Allen Guan <apguan@gmail.com>
2016-10-01 11:44:46 -0700

    microphone assets and scripts

---
+++ Assets/NewBehaviourScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+// Start recording with built-in Microphone and play the recorded audio right away
+	var aud = GetComponent.<AudioSource>();
+	aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+	aud.Play();
+
+
+//to see the device Unity is using 
+	for (var device in Microphone.devices){
+			Debug.Log("Name: " + device);
+	}
+}
+
+function Update () {
+
+}
